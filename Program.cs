020c4c1d7ad902e4bebf000feafefd969437cfa5
Alp Tuna Şahin <118995494+alpgithubx@users.noreply.github.com>
2023-07-08 15:33:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongSayıları {
+    internal class Program {
+        static void Main(string[] args) {
+            for(int i = 0; i < 1000000; i++) {
+                string str = Convert.ToString(i);
+                int length = str.Length;
+                char[] arr = str.ToCharArray();
+                List<int> list = new List<int>();
+                foreach (char c in arr) {
+                    list.Add
+                        ((int)Math.Pow(Convert.ToInt32(c.ToString()), length));
+                }
+                
+                 
+                if (list.Sum() == Convert.ToInt32(str)) {
+                    Console.WriteLine(list.Sum());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
